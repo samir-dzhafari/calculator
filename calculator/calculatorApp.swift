b721f627c94ec2ca3b977957ca6c36c7ddd3a2ee
Samir Dzhafari <samirjafari2001@icloud.com>
2023-09-20 17:06:58 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Самир Джафари on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
